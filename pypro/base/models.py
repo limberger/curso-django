39340c79091ddf0ca3c38ec329fc2410be4bf40e
Joao Paulo Limberger - SHOO <jlimberger@gmail.com>
2020-06-23 12:21:58 -0300

    Customizaćão da classe user - Criando a Usermanager Acertos flake8

--- pypro/base/models.py
+++ pypro/base/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
+
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
@@ -93,4 +94,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     def email_user(self, subject, message, from_email=None, **kwargs):
         """Send an email to this user."""
         send_mail(subject, message, from_email, [self.email], **kwargs)
-
