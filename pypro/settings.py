0b1b3b3e79121f0cae61998158a859834ff49443
Joao Paulo Limberger - SHOO <jlimberger@gmail.com>
2020-06-23 13:30:06 -0300

    Customizaćão da classe user - Criando a Usermanager Corrigindo nome do módulo collectfast

--- pypro/settings.py
+++ pypro/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'Collectfast',
+    'collectfast',
     'django.contrib.staticfiles',
     'pypro.base',
 ]
